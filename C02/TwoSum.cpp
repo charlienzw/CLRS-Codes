9f3adecfff913873dea7e347547320c3984c8063
charlienzw <charlienzw@gmail.com>
2018-06-01 18:23:41 +0800

    1

--- C02/TwoSum.cpp
+++ C02/TwoSum.cpp
@@ -1,13 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int BinarySearch(vector<int> A, int x)
+{
+	// A should be a sorted nondecreasing sequence.
+	int low = 0;
+	int high = A.size() - 1;
+	while (low <= high)
+	{
+		int mid = (low + high) / 2;
+		if (A[mid] == x)
+			return mid;
+		else if (A[mid] < x)
+			low = mid + 1;
+		else high = mid - 1;
+	}
+	return -1;
+}
+
 void Merge(vector<int> &A, int p, int q, int r)
 {
 	int n1 = q - p + 1;
 	int n2 = r - q;
 	vector<int> L(n1 + 1);
 	vector<int> R(n2 + 1);
-	int i,j;
+	int i, j;
 	for (i = 0; i < n1; i++)
 		L[i] = A[p + i];
 	for (j = 0; j < n2; j++)
@@ -37,14 +54,27 @@ void MergeSort(vector<int> &A, int p, int r)
 	{
 		int q = (p + r) / 2;
 		MergeSort(A, p, q);
-		MergeSort(A, q+1, r);
+		MergeSort(A, q + 1, r);
 		Merge(A, p, q, r);
 	}
 }
 
+bool TwoSum(vector<int> A, int x)
+{
+	int j;
+	MergeSort(A, 0, A.size() - 1);
+	for (int i = 0; i < A.size(); i++)
+	{
+		j = BinarySearch(A, x - A[i]);
+		if (i != j && j != -1)
+			return true;
+	}
+	return false;
+}
+
 int main()
 {
-	int N;
+	int N,x;
 	while (scanf("%d", &N) != EOF)
 	{
 		vector<int> A(N);
@@ -52,9 +82,8 @@ int main()
 		{
 			scanf("%d", &A[i]);
 		}
-		MergeSort(A, 0, A.size() - 1);
-		for (int i = 0; i<A.size(); i++)
-			printf("%d ", A[i]);
+		scanf("%d", &x);
+		printf("%d",TwoSum(A, x));
 		printf("\n");
 	}
 	return 0;
