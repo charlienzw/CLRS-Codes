9cdd1d78418a980c0c74012bdbb7566f72852fa4
charlienzw <charlienzw@gmail.com>
2018-06-19 00:55:21 +0800

    4

--- C04/FindMaxSubarray.cpp
+++ C04/FindMaxSubarray.cpp
@@ -30,7 +30,7 @@ void FindMaxCrossingSubarray(vector<int> &result, vector<int> &A, int low,int mi
 	result = { max_left,max_right,left_sum + right_sum };
 }
 
-void FindMaxSubarray(vector<int> &result,vector<int> &A,int low,int high)
+void FindMaxSubarray_Recursive(vector<int> &result,vector<int> &A,int low,int high)
 {
 	if (high == low)
 	{
@@ -40,9 +40,9 @@ void FindMaxSubarray(vector<int> &result,vector<int> &A,int low,int high)
 	{
 		int mid = (low + high) / 2;
 		vector<int> left_result(3);
-		FindMaxSubarray(left_result, A, low, mid);
+		FindMaxSubarray_Recursive(left_result, A, low, mid);
 		vector<int> right_result(3);
-		FindMaxSubarray(right_result, A, mid+1, high);
+		FindMaxSubarray_Recursive(right_result, A, mid+1, high);
 		vector<int> cross_result(3);
 		FindMaxCrossingSubarray(cross_result, A, low, mid,high);
 		if (left_result[2] >= right_result[2] && left_result[2] >= cross_result[2])
@@ -60,16 +60,104 @@ void FindMaxSubarray(vector<int> &result,vector<int> &A,int low,int high)
 	}
 }
 
+void FindMaxSubarray_BruteForce(vector<int> &result, vector<int> &A,int low,int high)
+{
+	int maxsum = INT_MIN;
+	int maxleft = 0;
+	int maxright = 0;
+	for (int i = low; i <=high; i++)
+	{
+		int sum = 0;
+		for (int j = i; j <=high; j++)
+		{
+			sum = sum + A[j];
+			if (sum > maxsum)
+			{
+				maxsum = sum;
+				maxleft = i;
+				maxright = j;
+			}
+		}
+	}
+	result = { maxleft,maxright,maxsum };
+}
+
+void FindMaxSubarray(vector<int> &result, vector<int> &A, int low, int high,int n0)
+{
+	if (high - low + 1 < n0)
+	{
+		FindMaxSubarray_BruteForce(result, A, low, high);
+	}
+	else
+	{
+		FindMaxSubarray_Recursive(result, A,low, high);
+	}
+}
+
+void FindMaxSubarray_emptyresult(vector<int> &result, vector<int> &A, int low, int high, int n0)
+{
+	if (high - low + 1 < n0)
+	{
+		FindMaxSubarray_BruteForce(result, A, low, high);
+	}
+	else
+	{
+		FindMaxSubarray_Recursive(result, A, low, high);
+	}
+	if (result[2] < 0)
+	{
+		result={ NULL,NULL,0 };
+	}
+}
+
+void FindMaxSubarray_lineartime(vector<int> &result, vector<int> &A)
+{
+	int maxsum = A[0];
+	int maxleft = 0;
+	int maxright = 0;
+	int j=0;
+	vector<int> sum_vec(A.size());
+	sum_vec[0] = A[0];
+	for (int i = 1; i < A.size(); i++)
+	{
+		if (sum_vec[i - 1] > 0)
+		{
+			sum_vec[i] = sum_vec[i - 1] + A[i];
+		}
+		else
+		{
+			sum_vec[i] = A[i];
+			j = i;
+		}
+		if (sum_vec[i] > maxsum)
+		{
+			maxsum = sum_vec[i];
+			maxleft = j;
+			maxright = i;
+		}
+	}
+	result = { maxleft,maxright,maxsum };
+}
+
 int main()
 {
 	int N;
+	int n0=44;
 	while (scanf("%d", &N) != EOF)
 	{
 		vector<int> A(N);
 		for (int i = 0; i<A.size(); i++)
 			scanf("%d", &A[i]);
 		vector<int> result(3);
-		FindMaxSubarray(result,A,0,N-1);
+		FindMaxSubarray(result,A,0,N-1,n0);
+		for (int i = 0; i<result.size(); i++)
+			printf("%d ", result[i]);
+		printf("\n");
+		FindMaxSubarray_emptyresult(result, A, 0, N - 1, n0);
+		for (int i = 0; i<result.size(); i++)
+			printf("%d ", result[i]);
+		printf("\n");
+		FindMaxSubarray_lineartime(result, A);
 		for (int i = 0; i<result.size(); i++)
 			printf("%d ", result[i]);
 		printf("\n");
